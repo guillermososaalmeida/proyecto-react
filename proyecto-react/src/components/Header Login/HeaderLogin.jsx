82bf607217dbcb19f89f4e5309a5844fee0b5720
guillebags <guillebags@gmail.com>
2023-09-02 23:04:49 +0200

    bugs de display

--- proyecto-react/src/components/Header Login/HeaderLogin.jsx
+++ proyecto-react/src/components/Header Login/HeaderLogin.jsx
@@ -1,18 +1,9 @@
-import {
-  Button,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  HStack,
-  Avatar,
-} from "@chakra-ui/react";
+import { Button, HStack } from "@chakra-ui/react";
 
 import "./HeaderLogin.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const HeaderLogin = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="HeaderLoginContainer">
