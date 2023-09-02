36013958ba07c55705dc6cc09828ac1f607e9cab
thelittlewild <thelittlewild@gmail.com>
2023-09-02 10:28:18 +0200

    arreglo anchura del login, estrutura del welcome

--- proyecto-react/src/pages/Welcome/Welcome.jsx
+++ proyecto-react/src/pages/Welcome/Welcome.jsx
@@ -1,16 +1,31 @@
 import "./Welcome.css";
-import { Link, HStack } from "@chakra-ui/react";
-
 import { useNavigate } from "react-router-dom";
 
 export const Welcome = () => {
   const navigate = useNavigate();
 
+  //array de imágenes (unas 10)
+  //abajo en carrusel poner las imágenes
+
   return (
     <>
       <div className="welcomeBody">
         <div className="HeaderWelcomeContainer">
-          <button className="btn">Register</button>
+          <button className="btn-login">Login</button>
+          <h1>Todos tus juegos a la alcance de tu mano</h1>
+          <p>Únete ya para poder tener todos tus juegos en un solo lugar </p>
+          <button className="btn-register">Register</button>
+        </div>
+        <div className="CarruselContainer">Carrusel</div>
+        <div className="PromotionContainer">
+          <div className="PromotionImg"></div>
+          <div className="PromotionText"></div>
+
+          <div className="PromotionText"></div>
+          <div className="PromotionImg"></div>
+
+          <div className="PromotionImg"></div>
+          <div className="PromotionText"></div>
         </div>
       </div>
     </>
